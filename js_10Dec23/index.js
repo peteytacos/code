ef2a80202ca75e66e8aea4cb7b3412492dab0f77
Pete <p.wallroth@yahoo.com>
2023-12-13 10:27:52 +1100

    13Dec23-1028

--- js_10Dec23/index.js
+++ js_10Dec23/index.js
@@ -1,3 +1,11 @@
-    //Writing some JavaScript code
-console.log('Hello World');
-    
+
+function square(number) {
+    return number + number;
+}
+
+console.log(square(2342343));
+
+document.querySelector('#button').addEventListener('click', function () {
+    let askTime = prompt("Please provide the time:");
+    return null;
+});
